8503cd31c66978ba57c7357618923da7f2e99c06
amodeose <amodeosa@gmail.com>
2022-08-06 21:20:51 -0400

    Updates

---
+++ precache-manifest.e108f9a457a55949f06d85b7b2733dbc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5466dd69ea40c3dae69d",
+    "url": "/Minesweeper-React/static/css/main.3616288f.chunk.css"
+  },
+  {
+    "revision": "5466dd69ea40c3dae69d",
+    "url": "/Minesweeper-React/static/js/main.5466dd69.chunk.js"
+  },
+  {
+    "revision": "f715b2e0bea123466b14",
+    "url": "/Minesweeper-React/static/js/1.f715b2e0.chunk.js"
+  },
+  {
+    "revision": "1f6a6ede6ea3eeb78b49",
+    "url": "/Minesweeper-React/static/js/runtime~main.1f6a6ede.js"
+  },
+  {
+    "revision": "374344147cff8606f835d752944d8570",
+    "url": "/Minesweeper-React/static/media/light-grey-terrazzo.37434414.png"
+  },
+  {
+    "revision": "c95453298f1d02a73df6e4ba828ff29d",
+    "url": "/Minesweeper-React/index.html"
+  }
+];
